ae6c817e985bf7fb82171d8c12841b5f0425958f
abuXahra <abdulmuminisah79@gmail.com>
2022-06-02 14:00:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@ include <stdio.h>
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle,\n";)
+	putsi("Programming is like building a multilingual puzzle\n");
 	return 0;	
 }
