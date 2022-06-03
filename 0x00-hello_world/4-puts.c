4f2282d59618d4c5def85385ffe765de745dd719
abuXahra <abdulmuminisah79@gmail.com>
2022-06-03 01:34:45 -0700

    4 puts fucntion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
-#!/bin/bash
 include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 
-	/**
-	 * main - Entry point
-	 *
-	 * Return: Always 0 (Success)
-	 */
+	puts("\"Programming is like building a multilingual puzzle");
 
-	putsi("\"Programming is like building a multilingual puzzle");
 	return 0;	
 
 }
