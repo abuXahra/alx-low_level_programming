bb5ec154d6913bf8fe3b35c60ce534dde2c13b05
abuXahra <abdulmuminisah79@gmail.com>
2022-06-07 02:24:17 -0700

    print number in base 10 starting from zero

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Isah Abdulmumin
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; letter < 10; n++)
+	{
+		printf("%d\n", n);
+	}
+
+	return (0);
+}
