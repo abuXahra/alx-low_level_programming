b476a7495c31e721c67ac52aa2b3c29604d7fb4f
abuXahra <abdulmuminisah79@gmail.com>
2022-06-07 03:03:27 -0700

    print numbers in base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -15,8 +15,13 @@ int main(void)
 {
 	int num;
 
+	char letter
+
 	for (num = 0; num < 16; num++)
 		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
 
 	putchar('\n');
 
