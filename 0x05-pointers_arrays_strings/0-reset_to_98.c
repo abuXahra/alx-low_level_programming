c91966c7d66af837718a7d981a181012d0680572
abuXahra <abdulmuminisah79@gmail.com>
2022-06-16 02:10:59 -0700

    function that pointer to an Int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,28 +3,14 @@
  * Auth: Isah Abdulmumin
  */
 
-#include "stdio.h"
+#include "alx.h"
 
 /**
  * reset_to_98 - Takes a pointer to an int and updates
  *               the value it points to to 98.
  * @n: The pointer to an int.
  */
-
-
-void reset_to_98(int *n){
-
-	*n = 98;
-}
-
-int main()
+void reset_to_98(int *n)
 {
-
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+	*n = 98;
 }
